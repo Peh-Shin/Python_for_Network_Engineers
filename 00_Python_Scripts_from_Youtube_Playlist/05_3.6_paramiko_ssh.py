b7e22045965e47a491819ece94f3b13d68d008db
= <=>
2025-03-27 11:55:55 +0800

    update SSH script to prompt for password and use a specific username

--- 00_Python_Scripts_from_Youtube_Playlist/05_3.6_paramiko_ssh.py
+++ 00_Python_Scripts_from_Youtube_Playlist/05_3.6_paramiko_ssh.py
@@ -1,15 +1,23 @@
+##############################################################################################################
+# 1. This script is to connect to a remote device using paramiko library.
+# 2. The script will prompt for a password to connect to the device.
+# 3. The script will then connect to the device and run the command.
+# 4. The output of the command will be printed to the screen.
+# 5. The script will then close the connection to the device.
+##############################################################################################################
+
 #! /data/05_Atom_Demo/Atom_P3.6/bin/python3.6
 import paramiko
 import time
 from getpass import getpass
 ip = '192.168.18.10'
-# username = 'admin'
-password = 'cisco'
+username = 'pshin'
+password = getpass()
 
 SESSION = paramiko.SSHClient()
 SESSION.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 SESSION.connect(ip,port=22,
-                #username=username,
+                username="pshin",
                 password=password,
                 look_for_keys=False,
                 allow_agent=False)
